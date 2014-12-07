a3d2738aaaebebdeca8d8376c0592f485cc02301
squit <michael.squitieri@gmail.com>
2014-12-07 00:59:55 -0500

    sessions controller now returns proper data

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,19 +7,19 @@ class Api::V1::SessionsController < Api::V1::ApiController
 
   def create
     sign_in user
-    respond_with user
+    render json: { id: user.id, email: user.email }
   end
 
   def destroy
     sign_out :user
-    render json: {}, status: :accepted
+    render nothing: true, status: :accepted
   end
 
   def current
     if current_user.present?
       respond_with current_user
     else
-      render json: {}
+      render_message(:session, 404, "No current user session.")
     end
   end
 
@@ -29,7 +29,7 @@ class Api::V1::SessionsController < Api::V1::ApiController
     @user = User.find_for_database_authentication(email: params[:session][:email])
 
     unless user && user.valid_password?(params[:session][:password])
-      render_message(:session, "Invalid Email or Password", 422)
+      render_message(:session, 422, "Invalid Email or Password")
     end
   end
 
