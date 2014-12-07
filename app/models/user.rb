4e658842ab682b95a71387fd91bd168598ccd297
squit <michael.squitieri@gmail.com>
2014-12-07 00:40:59 -0500

    added dependent: destroy to models and removed confirmable devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :lists
+  has_many :lists, dependent: :destroy
 end
