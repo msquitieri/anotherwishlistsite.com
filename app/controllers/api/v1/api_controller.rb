8d979d629f46e08ac548c2aa5189dcf13710ad7e
squit <michael.squitieri@gmail.com>
2014-12-14 01:16:24 -0500

    now make sure to pass api key

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  before_action :authenticate_api_key
+
   def render_message(head, status_code, message)
     render json: { head => { message: message } }, status: status_code
+  end
+
+  private
+
+  def authenticate_api_key
+    api_key = params[:api_key] || request.headers['X-api-key']
+    render_message(:auth, 401, 'Invalid API key') if api_key.nil? or api_key != '1234'
   end
 
 end
