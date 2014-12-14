213dadaa094c261420339feb5abe2d7dddf8b1b8
squit <michael.squitieri@gmail.com>
2014-12-13 22:38:02 -0500

    initial ember commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require ./anotherwishlistsite_com
+
+// for more details see: http://emberjs.com/guides/application/
+AnotherwishlistsiteCom = Ember.Application.create();
+
 //= require_tree .
---
+++ app/assets/javascripts/store.js
@@ -0,0 +1,9 @@
+AnotherwishlistsiteCom.Store = DS.Store.extend({
+
+});
+
+// Override the default adapter with the `DS.ActiveModelAdapter` which
+// is built to work nicely with the ActiveModel::Serializers gem.
+AnotherwishlistsiteCom.ApplicationAdapter = DS.ActiveModelAdapter.extend({
+
+});
