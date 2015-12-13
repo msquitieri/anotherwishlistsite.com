a8b1b49a3de60f015242957ffddd0d4a6886b007
Michael Squitieri <michael.squitieri@gmail.com>
2015-12-13 00:47:09 -0500

    move to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -13,51 +13,48 @@
 
 ActiveRecord::Schema.define(version: 20141214033045) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "items", force: true do |t|
-    t.string   "title"
-    t.string   "price"
-    t.string   "notes"
-    t.string   "image"
-    t.string   "link"
-    t.integer  "quantity"
-    t.integer  "priority"
-    t.integer  "list_id"
+  create_table "items", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "price",      limit: 255
+    t.string   "notes",      limit: 255
+    t.string   "image",      limit: 255
+    t.string   "link",       limit: 255
+    t.integer  "quantity",   limit: 4
+    t.integer  "priority",   limit: 4
+    t.integer  "list_id",    limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "reserved",   default: false
+    t.boolean  "reserved",               default: false
   end
 
   add_index "items", ["list_id"], name: "index_items_on_list_id", using: :btree
 
-  create_table "lists", force: true do |t|
-    t.string   "title"
+  create_table "lists", force: :cascade do |t|
+    t.string   "title",      limit: 255
     t.date     "start_date"
     t.date     "end_date"
-    t.integer  "user_id"
+    t.integer  "user_id",    limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "lists", ["user_id"], name: "index_lists_on_user_id", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "first_name"
-    t.string   "last_name"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
