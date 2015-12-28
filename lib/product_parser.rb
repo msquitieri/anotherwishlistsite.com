3e0e40878d9b5f80129036b34c4e878e7374da18
Michael Squitieri <michael.squitieri@gmail.com>
2015-12-28 00:15:48 -0500

    ensure that the images urls are returned in the correct format

--- lib/product_parser.rb
+++ lib/product_parser.rb
@@ -3,6 +3,8 @@ require 'open-uri'
 class ProductParser
   attr_reader :url, :title, :images
 
+  MIN_IMAGE_WIDTH = 100
+
   USER_AGENT_STRINGS = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0",
@@ -31,9 +33,38 @@ class ProductParser
     @doc     = Nokogiri::HTML(response.body)
 
     @title  = doc.xpath('//title').first.content.to_s
-    @images = doc.xpath('//img').select { |img| img[:width].to_i > 100 }.map { |img| img.attr('src') }
+    @images = find_images
 
     self
+  end
+
+  def find_images
+    # Select all images that have a width > MIN_IMAGE_WIDTH.
+    # If the width is not present, take it anyway.
+    all_images = doc.xpath('//img').select do |img|
+      if img[:width].present?
+        img[:width].to_i > MIN_IMAGE_WIDTH
+      else
+        true
+      end
+    end
+
+    all_images.map { |img| decorate_image_url(url, img.attr('src')) }
+  end
+
+  def decorate_image_url(base_url, image_url)
+    uri = URI.parse(base_url)
+
+    # Case 1: '//s3.amazonaws.com/bucket' => 'https://s3.amazonaws.com/bucket'
+    # Case 2: '/images/product-image.png' => 'https://amazon.com/images/product-image.png'
+    if image_url.start_with?('//')
+      "#{uri.scheme}:#{image_url}"
+    elsif image_url.start_with?('/')
+      "#{uri.scheme}://#{uri.host}#{image_url}"
+    else
+      image_url
+    end
+
   end
 
 
